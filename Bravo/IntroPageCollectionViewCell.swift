372a77b7b44a837835c7311707b380f9e088a464
kurt ho <ahopdanzer@gmail.com>
2016-08-12 15:06:49 +0800

    fixing collection view cell

---
+++ Bravo/IntroPageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  IntroPageCollectionViewCell.swift
+//  Bravo
+//
+//  Created by KurtHo on 2016/8/12.
+//  Copyright © 2016年 Kurt. All rights reserved.
+//
+
+import UIKit
+
+class IntroPageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+
+    
+
+}
--- Bravo/IntroViewController.swift
+++ Bravo/IntroViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class IntroViewController: UIViewController {
-
     @IBOutlet weak var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
     }
 
@@ -27,19 +28,36 @@ class IntroViewController: UIViewController {
 
 
 extension IntroViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return IntroContent.contents.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("myCell", forIndexPath: indexPath) as! IntroCollectionViewCell
-        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("myCell", forIndexPath: indexPath) as! IntroPageCollectionViewCell
+        cell.myImage.image = UIImage(named: IntroContent.contents[indexPath.row].myImage)
+        cell.myLabel.text = IntroContent.contents[indexPath.row].myLabel
         return cell
     }
     
+}
+
+
+
+class IntroContent {
+    static let contents = [
+        IntroContent(myImage: "moon", myLabel: "測試一"),
+        IntroContent(myImage: "me", myLabel: "測試二"),
+        IntroContent(myImage: "moon", myLabel: "測試三")
+    ]
+    let myImage: String
+    let myLabel: String
+    
+    init(myImage: String, myLabel: String ) {
+        self.myImage = myImage
+        self.myLabel = myLabel
+    }
 }
